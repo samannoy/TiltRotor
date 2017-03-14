d4cfab1a533602664ee49369c1d7fe8666c6a70b
Samannoy Ghosh <prince.sam1495@gmail.com>
2017-03-15 01:25:43 +0530

    Add files via upload

---
+++ ArduPlane/capabilities.cpp
@@ -0,0 +1,14 @@
+// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+#include "Plane.h"
+
+void Plane::init_capabilities(void)
+{
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_MISSION_FLOAT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_PARAM_FLOAT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_COMMAND_INT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_MISSION_INT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT);
+    hal.util->set_capabilities(MAV_PROTOCOL_CAPABILITY_SET_ATTITUDE_TARGET);
+
+}
